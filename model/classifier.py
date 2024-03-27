a0d6b61dc05646712c4af0e577e5e05fe87b47a1
rorzz <rorzz2@mail.ru>
2024-03-27 13:24:48 +0000

    started

--- model/classifier.py
+++ model/classifier.py
@@ -25,46 +25,49 @@ cam = cv2.VideoCapture(0)
 # return true if the current posture is bad
 # if logs = True, prints logs
 def is_bad_posture(logs=False):
-    result, img_read = cam.read()
+    try:
+        result, img_read = cam.read()
 
-    # if there is a problem with a camera
-    if not result:
+        # if there is a problem with a camera
+        if not result:
+            return False
+        
+        # save the image
+        cv2.imwrite("image.png", img_read)
+
+        # Replace this with the path to your image
+        image = Image.open("image.png").convert("RGB")
+
+        # resizing the image to be at least 224x224 and then cropping from the center
+        size = (224, 224)
+        image = ImageOps.fit(image, size, Image.LANCZOS)
+
+        # turn the image into a numpy array
+        image_array = np.asarray(image)
+
+        # Normalize the image
+        normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+
+        # Load the image into the array
+        data[0] = normalized_image_array
+
+        # Predicts the model
+        prediction = model.predict(data)
+        prediction_index = np.argmax(prediction)
+        predicted_class = class_names[prediction_index]
+        confidence_score = prediction[0][prediction_index]
+
+        predicted_classname = predicted_class[2:]
+
+        if(logs):
+            print("Class:", predicted_classname, end="")
+            print("Confidence Score:", confidence_score)
+
+        if(prediction_index == 1):
+            return True
+        
         return False
-    
-    # save the image
-    cv2.imwrite("image.png", img_read)
-
-    # Replace this with the path to your image
-    image = Image.open("image.png").convert("RGB")
-
-    # resizing the image to be at least 224x224 and then cropping from the center
-    size = (224, 224)
-    image = ImageOps.fit(image, size, Image.LANCZOS)
-
-    # turn the image into a numpy array
-    image_array = np.asarray(image)
-
-    # Normalize the image
-    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
-
-    # Load the image into the array
-    data[0] = normalized_image_array
-
-    # Predicts the model
-    prediction = model.predict(data)
-    prediction_index = np.argmax(prediction)
-    predicted_class = class_names[prediction_index]
-    confidence_score = prediction[0][prediction_index]
-
-    predicted_classname = predicted_class[2:]
-
-    if(logs):
-        print("Class:", predicted_classname, end="")
-        print("Confidence Score:", confidence_score)
-
-    if(prediction_index == 1):
-        return True
-    
-    return False
+    except Exception as e:
+        print("Error while classifying:", e)
 
 
--- model/starter_ai.py
+++ model/starter_ai.py
@@ -6,35 +6,29 @@ cur_loop_thread = None
 is_started = False
 
 def classify_loop():
-    while(is_started):
-        if(is_bad_posture()):
-            print("bad")
-        else:
-            print("good")
-        sleep(1)
+    try:
+        while(is_started):
+            if(is_bad_posture()):
+                print("bad")
+            else:
+                print("good")
+            sleep(1)
+    except Exception as e:
+        print("Error in classifying loop:", e) 
 
 
 def start():
     global cur_loop_thread, is_started
 
+    is_started = True
+    
     if(cur_loop_thread != None):
-        cur_loop_thread.join(0)
-
+        cur_loop_thread.join()
+        
     cur_loop_thread = Thread(target=classify_loop)
     cur_loop_thread.start()
 
-    is_started = True
 
 def stop():
     global is_started
     is_started = False
-
-
-start()
-sleep(5)
-stop()
-print("stopped")
-sleep(10)
-start()
-sleep(10)
-stop()
