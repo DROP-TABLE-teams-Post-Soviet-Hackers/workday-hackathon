cf379967022c13f0b60bd479da9b5371ec66833f
turnMeUpSon <milleryan2003@gmail.com>
2024-03-27 15:53:43 +0000

    Created notifications

--- model/starter_ai.py
+++ model/starter_ai.py
@@ -1,6 +1,7 @@
 from threading import Thread
 from time import sleep
 
+import notifypy
 import plyer
 from classifier import is_bad_posture, posture_checker
 
@@ -8,7 +9,28 @@ cur_loop_thread = None
 is_started = False
 
 
-def notif_with_plyer(title: str, message: str):
+def notif_with_notifpy(title: str, message: str):
+    """
+    Sends a notification using the notif-py engine.
+
+    Args:
+        title: str: the title of the notification
+        message: str: the message of the notification
+    Note:
+        Cross-Platform: could be used on any OS.
+    """
+    notification = notifypy.Notify(enable_logging=True)  # I like enabling logging :)
+    notification.application_name = "Hello, your posture is bad now!"
+    notification.title = title
+    notification.message = message
+    notification.urgency = "critical"
+
+    notification.send(
+        block=False
+    )  # block=False spawns a separate thread inorder not to block the main app thread
+
+
+'''def notif_with_plyer(title: str, message: str):
     """
     Sends a notification using the plyer engine.
 
@@ -24,16 +46,21 @@ def notif_with_plyer(title: str, message: str):
         title=title,
         message=message,
         timeout=10,
-    )
+    )'''
 
 
 def classify_loop():
     try:
         while is_started:
             if is_bad_posture():
-                notif_with_plyer(
+                """notif_with_plyer(
                     title="Hello, your posture is bad now!", message="Improve it"
+                )"""
+                notif_with_notifpy(
+                    title="Hello, your posture is bad now!",
+                    message="Improve it",
                 )
+
             else:
                 print("good")
             sleep(0.2)
