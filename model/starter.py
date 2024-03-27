c2d44c842dad2b17c045dbb736043e22d84e539f
rorzz <rorzz2@mail.ru>
2024-03-27 13:19:26 +0000

    progress

--- model/starter.py
+++ model/starter.py
@@ -2,14 +2,39 @@ from threading import Thread
 from classifier import is_bad_posture
 from time import sleep
 
-def start_classifiying():
-    while(True):
+cur_loop_thread = None
+is_started = False
+
+def classify_loop():
+    while(is_started):
         if(is_bad_posture()):
             print("bad")
         else:
             print("good")
-        sleep(5)
+        sleep(1)
 
 
-thread = Thread(target=start_classifiying)
-thread.start()
+def start():
+    global cur_loop_thread, is_started
+
+    if(cur_loop_thread != None):
+        cur_loop_thread.join(0)
+
+    cur_loop_thread = Thread(target=classify_loop)
+    cur_loop_thread.start()
+
+    is_started = True
+
+def stop():
+    global is_started
+    is_started = False
+
+
+start()
+sleep(5)
+stop()
+print("stopped")
+sleep(10)
+start()
+sleep(10)
+stop()
